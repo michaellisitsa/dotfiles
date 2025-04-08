5f3758b8ac409bd9c6cc23ae5f4e7c9d5d6c5fe8
Michael Lisitsa <michael.lisitsa@gmail.com>
2025-04-09 09:56:34 +1000

    Re-enable treesitter textobjects and mini.ai - allows for working with whitespace languages like python   with smarter treesitter object

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -2,10 +2,12 @@ return {
   -- Too much magic with the command line
   { "folke/noice.nvim", enabled = false },
   -- Don't use extra a and i motions
-  { "echasnovski/mini.ai", enabled = false },
+  -- Re-enabling for white-space languages (Python)
+  -- { "echasnovski/mini.ai", enabled = false },
   -- Pairs often cause problems particularly on command line when doing searches and substitutions
   -- but they are useful for normal coding.
   -- { "echasnovski/mini.pairs", enabled = false },
   -- There are a lot of motions I haven't used
-  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
+  -- Re-enabling for white-space languages (Python)
+  -- { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
 }
