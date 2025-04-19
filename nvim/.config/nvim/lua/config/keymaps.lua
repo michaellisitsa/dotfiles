4fb27c7366d59ba23fdd94d8398fc74ff4792cff
Michael Lisitsa <michael@lisitsa.dev>
2025-04-19 13:33:15 +1000

    Add keybind for toggling virtual lines - new feature in vim 0.11 useful when on current line

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Virtual lines take up too much space with the LSP diagnostics
+-- Showing at current line only is also distracting
+-- Instead just have a toggle.
+-- When lines are on, text is off.
+vim.keymap.set("", "<leader>bl", function()
+  vim.diagnostic.config({
+    virtual_lines = not vim.diagnostic.config().virtual_lines,
+    virtual_text = not vim.diagnostic.config().virtual_text,
+  })
+end, { desc = "Toggle diagnostic [l]ines" })
