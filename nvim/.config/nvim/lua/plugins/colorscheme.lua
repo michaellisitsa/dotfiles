d300a6c7dd082da05f70f61f6a4a12c0e6b0b31b
Michael Lisitsa <michael@lisitsa.dev>
2025-04-19 13:38:47 +1000

    Clean up color scheme code

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,9 @@
--- return {
---   -- add nightfox
---   { "EdenEast/nightfox.nvim" },
---
---   -- Configure LazyVim to load nightfox
---   {
---     "LazyVim/LazyVim",
---     opts = {
---       colorscheme = "nightfox",
---     },
---   },
--- }
-
 return {
   -- add vscode theme
   --https://github.com/Mofiqul/vscode.nvim
   { "Mofiqul/vscode.nvim" },
 
-  -- Configure LazyVim to load nightfox
+  -- Configure LazyVim to load vscode theme
   {
     "LazyVim/LazyVim",
     opts = {
