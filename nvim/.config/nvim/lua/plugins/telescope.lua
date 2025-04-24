20a1aad074ffaf127a2a46d689394784a6d0590d
Michael Lisitsa <michael@lisitsa.dev>
2025-04-24 20:44:54 +1000

    Change to telescope + live grep with args - vim motions inside search box, better syntax highlighting

---
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,57 @@
+return {
+  -- Config to extend Telescope with live grep args.
+  -- Useful for grepping through hidden files that are committed to git, e.g. .env .gitignore
+  -- c-k mapping is the standard one for toggling argument mode. Alternatively just surround in quotes
+  -- https://www.reddit.com/r/neovim/comments/1bjs39r/comment/kvvsq5b/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+      },
+    },
+    opts = function(_, opts)
+      local lga_actions = require("telescope-live-grep-args.actions")
+      opts.extensions = {
+        live_grep_args = {
+          auto_quoting = true, -- enable/disable auto-quoting
+          -- define mappings, e.g.
+          mappings = { -- extend mappings
+            i = {
+              ["<C-k>"] = lga_actions.quote_prompt(),
+              ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+            },
+          },
+          -- ... also accepts theme settings, for example:
+          -- theme = "dropdown", -- use dropdown theme
+          -- theme = { }, -- use own theme spec
+          -- layout_config = { mirror=true }, -- mirror preview pane
+        },
+      }
+      opts.defaults = vim.tbl_extend("force", opts.defaults, {
+        mappings = {
+          i = {
+            ["<C-j>"] = require("telescope.actions").move_selection_next,
+            ["<C-k>"] = require("telescope.actions").move_selection_previous,
+          },
+          n = { -- while in normal mode
+            ["<C-j>"] = require("telescope.actions").move_selection_next,
+            ["<C-k>"] = require("telescope.actions").move_selection_previous,
+          },
+        },
+      })
+    end,
+    keys = {
+      {
+        "<leader>/",
+        "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+        desc = "Grep (root dir)",
+      },
+    },
+    config = function(_, opts)
+      local tele = require("telescope")
+      tele.setup(opts)
+      tele.load_extension("live_grep_args")
+    end,
+  },
+}
