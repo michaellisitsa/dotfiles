630a1eba717b6e057851c3a5ece109a98c02ad52
Michael Lisitsa <michael@lisitsa.dev>
2025-04-26 12:45:11 +1000

    Remove lazyvim

--- nvim/.config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
--- Disable copilot globally.
--- To enable on a per case based use
--- `:Copilot enable`
--- Commented out for now because we've uninstalled the plugin
--- vim.cmd(":Copilot disable")
