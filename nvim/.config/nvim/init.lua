1b9ca98408916ce199d49a59d807a8127996cc99
Michael Lisitsa <michael@lisitsa.dev>
2025-04-27 21:43:01 +1000

    Ruff initial settings were not valid, remove

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -738,13 +738,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         pyright = {},
-        ruff = {
-          init_options = {
-            settings = {
-              -- Ruff language server settings go here
-            },
-          },
-        },
+        ruff = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
