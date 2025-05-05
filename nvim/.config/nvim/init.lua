76e20db5f898c64567f5071bb6668da7de775c3d
Michael Lisitsa <michael@lisitsa.dev>
2025-05-05 10:45:46 +1000

    Fix keymap clash nvim-surround & flash, telescope buffer sorts most recent - https://github.com/folke/flash.nvim/discussions/251 - https://github.com/nvim-telescope/telescope.nvim/issues/791

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -481,6 +481,7 @@ require('lazy').setup({
         -- but ignore dependency folders.
         -- Alternatively, we could use Live Grep with Arguments extension to dynamically modify these settings
         pickers = {
+          ['buffers'] = { sort_mru = true, ignore_current_buffer = true, sort_lastused = true, initial_mode = 'normal' },
           file_ignore_patterns = { 'node_modules', '.git', '.venv' },
         },
         defaults = {
@@ -966,7 +967,6 @@ require('lazy').setup({
   {
     'folke/flash.nvim',
     event = 'VeryLazy',
-    ---@type Flash.Config
     opts = {
       modes = {
         char = {
@@ -977,9 +977,7 @@ require('lazy').setup({
     -- stylua: ignore
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      -- Remote too complicated
-      -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "S", false, mode = { "v" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
