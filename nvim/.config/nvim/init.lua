26c60037f049090059dd943021ac8694d54f4e55
Michael Lisitsa <michael@lisitsa.dev>
2025-05-06 11:08:22 +1000

    Increase tmux buffer history, add treesitter for conditionals loops

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -333,12 +333,16 @@ require('lazy').setup({
 
             keymaps = {
               -- You can use the capture groups defined in textobjects.scm
+              ['al'] = '@loop.outer',
+              ['il'] = '@loop.inner',
+              ['ac'] = '@conditional.outer',
+              ['ic'] = '@conditional.inner',
               ['af'] = '@function.outer',
               ['if'] = '@function.inner',
-              ['ac'] = '@class.outer',
+              ['aC'] = '@class.outer',
               -- You can optionally set descriptions to the mappings (used in the desc parameter of
               -- nvim_buf_set_keymap) which plugins like which-key display
-              ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
+              ['iC'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
               -- You can also use captures from other query groups like `locals.scm`  "nvim-treesitter/nvim-treesitter-textobjects",
               ['as'] = { query = '@local.scope', query_group = 'locals', desc = 'Select language scope' },
             },
