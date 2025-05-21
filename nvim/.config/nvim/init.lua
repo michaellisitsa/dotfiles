1f7a408390acae17fe9353ec816aace92c653ddb
Michael Lisitsa <michael@lisitsa.dev>
2025-05-21 13:59:28 +1000

    Add dropbar to nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1082,6 +1082,22 @@ require('lazy').setup({
       }
     end,
   },
+  {
+    {
+      'Bekaboo/dropbar.nvim',
+      -- optional, but required for fuzzy finder support
+      dependencies = {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+      },
+      config = function()
+        local dropbar_api = require 'dropbar.api'
+        vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+        vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+        vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+      end,
+    },
+  },
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     -- Currently we only use status line which is not necessary
